d44ce532d34d1d116285f2a3fda404efdc0650c1
Turgay Serdaş <turgay.serdas@gmail.com>
2018-07-14 01:04:19 +0300

    v.2 - First Design

--- tRekt/MainWindow.xaml.cs
+++ tRekt/MainWindow.xaml.cs
@@ -24,5 +24,41 @@ namespace tRekt
         {
             InitializeComponent();
         }
+
+        private void tryclick(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+            GridCursor.Margin = new Thickness(10 + (150 * index), 0, 0, 0);
+
+            switch(index)
+            {
+                case 0:
+                    GridMain.Background = Brushes.White;
+                    break;
+                case 1:
+                    GridMain.Background = Brushes.SaddleBrown;
+                    break;
+                case 2:
+                    GridMain.Background = Brushes.RosyBrown;
+                    break;
+                case 3:
+                    GridMain.Background = Brushes.SandyBrown;
+                    break;
+                case 4:
+                    GridMain.Background = Brushes.SkyBlue;
+                    break;
+                case 5:
+                    GridMain.Background = Brushes.Snow;
+                    break;
+                case 6:
+                    GridMain.Background = Brushes.SlateGray;
+                    break;
+            }
+        }
+
+        private void btn_close(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
